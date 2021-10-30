219e6e00a502d13c62790a790828cff784b80c31
Kim9xd <kim9xd@gmail.com>
2021-10-30 15:53:00 +0900

    * 5일차  - 31.Headless 크롬

--- 17_headless_chrome.py
+++ 17_headless_chrome.py
@@ -1,18 +1,16 @@
 from selenium import webdriver
-browser = webdriver.Chrome()
+
+options = webdriver.ChromeOptions()
+options.headless = True
+options.add_argument("window-size=1920x1080")
+
+
+browser = webdriver.Chrome(options=options)
 browser.maximize_window()
 
 # 페이지 이동
 url = "https://play.google.com/store/movies/top"
 browser.get(url)
-
-# 지정한 위치로 스크롤 내리기
-# 모니터(해상도) 높이인 1080 위치로 스크롤 내리기
-# browser.execute_script("window.scrollTo(0, 1080)") # 1920 x 1080
-# browser.execute_script("window.scrollTo(0, 2080)") 
-
-# 화면 가장 아래로 스크롤 내리기
-# browser.execute_script("window.scrollTo(0, document.body.scrollHeight)") 
 
 
 import time
@@ -36,6 +34,7 @@ while True:
     prev_height = curr_height
 
 print("스크롤 완료")
+browser.get_screenshot_as_file("google.movie.png")
 
 
 import requests
